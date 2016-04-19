bb838926b8e4b93ee994ed7da31b57c1510a411f
moynes <martheoy@stud.ntnu.no>
2016-04-19 21:45:35 +0200

    Task 1.3 may be done now, but 2 not so much

--- foursquare.py
+++ foursquare.py
@@ -74,12 +74,26 @@ def unique_column_count(sc_file, col_n=0):
 
 
 def multi_column_count(sc_file):
-
     return sc_file.map(lambda row: (row[0], row[1], row[2])).groupByKey()
 
 
 def closest_city(lat, lon):
     return 1
+
+
+def task_3(foursqr, cities):
+    # checkin_id[0], lat[1], lon[2]
+    user_coordination_data = foursqr.map(lambda x: (x[0], x[5], x[6]))
+
+    # city[0], lat[1], lon[2], country[3]
+    city_coordination_data = cities.map(lambda x: (x[0], x[1], x[2], x[4]))
+
+    cart_user_city = user_coordination_data.cartesian(city_coordination_data).map(
+        lambda ((checkin_id, checkin_lat, checkin_lon), (city, lat, lon, country)): (
+        checkin_id, city, country, haversine(float(checkin_lat), float(checkin_lon), float(lat), float(lon)),
+        checkin_lat, checkin_lon)).filter(lambda (id, city, country, d, checkin_lat, checkin_lon): d < 1)
+
+    print(cart_user_city.collect())
 
 
 def task_4(foursqr, cities):
@@ -111,11 +125,8 @@ def task_5(foursqr):
 
 def task_6(foursqr):
     a = time.time()
-    selection = foursqr.map(lambda row: (row[2], {'pos': (float(row[5]), float(row[6]))})) \
-        .groupByKey() \
-        .filter(lambda row: len(row[1]) >= 4) \
-        .map(lambda row: (row[0], haversine_path_dict(list(row[1]))))\
-        .collect()
+    selection = foursqr.map(lambda row: (row[2], {'pos': (float(row[5]), float(row[6]))})).groupByKey().filter(
+        lambda row: len(row[1]) >= 4).map(lambda row: (row[0], haversine_path_dict(list(row[1])))).collect()
     print(selection)
     print('time 1:', time.time() - a)
 
@@ -128,7 +139,7 @@ def task_7(foursqr, df):
                                                   'timestamp': str(timestamp(row[3], row[4])),
                                                   'category': row[7],
                                                   'subcategory': row[8]}
-                                         ))\
+                                         )) \
         .groupByKey() \
         .map(lambda row: (row[0], row[1], haversine_path_dict(list(row[1])))) \
         .filter(lambda row: row[2] >= 50.0) \
@@ -147,6 +158,8 @@ def task_7(foursqr, df):
                                                         session_map['pos'][1],
                                                         session_map['category'],
                                                         session_map['subcategory']))
+
+    # TODO : output to .tsv/.csv file and visualize in CartoDB
 
     '''
     Implemented a reduceByKey version that is 4x as fast, however - it is also wrong as haversine demands ordered positions.
@@ -217,21 +230,23 @@ def task_7(foursqr, df):
                                                                           session_map['subcategory']))
     '''
 
+
 if __name__ == "__main__":
     sc = SparkContext(appName="Foursquare")
     sqlContext = SQLContext(sc)
 
-    print('Foursquare!')
-    print('Task 1 - load the dataset')
-    # Loading file
+    print('Task 1 - Exploratory Analysis of Foursquare Dataset')
+    print('Task 1.1 - load the dataset')
+
     # foursqr = sc.textFile('Foursquare_data/dataset_TIST2015.tsv')
     foursqr = sc.textFile('foursquare_sessions_example.tsv')
     header = foursqr.first()  # extract header
     foursqr = foursqr.filter(lambda x: x != header).map(lambda x: tuple(x.split('\t')))
-    print('foursquare loaded')
+    print('Foursquare loaded')
 
     cities_file = sc.textFile('foursquare_cities_example.txt').map(lambda x: tuple(x.split('\t')))
-    print('cities loaded')
+    print('Cities loaded')
+
     print('Creating SQL Context')
     # Creating SQL context
     cities = cities_file.map(lambda c: Row(name=c[0], lat=float(c[1]), lon=float(c[2]), country_code=c[3],
@@ -241,7 +256,9 @@ if __name__ == "__main__":
     cities_df.registerTempTable('cities')
     sqlContext.cacheTable('cities')
 
-    print('Task 2 - calculate local time for each checkin')  # see timestamp function
+    # TODO : define as function
+    # See timestamp function
+    print('Task 1.2 - Calculate local time for each checkin')
     checkins = foursqr.map(lambda l: Row(checkin_id=int(l[0]), user_id=int(l[1]), session_id=l[2],
                                          time=timestamp(l[3], l[4]), lat=float(l[5]), lon=float(l[6]), category=l[7],
                                          subcategory=l[8], city=closest_city(float(l[5]), float(l[6])))
@@ -251,28 +268,20 @@ if __name__ == "__main__":
     checkin_df.registerTempTable("checkins")
     print('SQL Context created')
 
-    print('Task 3')
-    print('needs to be done')
+    print('Task 1.3 - Assign a city and country to each check-in')
+    # task_3(foursqr, cities_file)
 
-    print('Task 4:')
+    print('Task 1.4 - bunch of questions')
     # task_4(foursqr, cities_file)
 
-    print('Task 5')
+    print('Task 1.5 - Calculate lengths of sessions as number of check-ins and provide a histogram')
     # task_5(foursqr)
+    # TODO: create histogram
 
-    print('Need to create some sort of histogram')
-
-    print('Task 6')
+    print('Task 1.6 - Calculate distance in km for sessions with 4 check-ins or more')
     # task__6(foursqr)
 
-    print('Task 7')
-    task_7(foursqr, checkin_df)
-
-    print('needs to be done')
-
-    print('Task 8')
-    print('optional')
-
-
+    print('Task 1.7 - Find 100 longest sessions')
+    # task_7(foursqr, checkin_df)
 
     sc.stop()
--- twitter.py
+++ twitter.py
@@ -1,22 +1,24 @@
 from __future__ import print_function
 from pyspark import SparkContext, SQLContext, Row
+import sys
 
 
 # - - - - - - - - - - - - - HELPER FUNCTIONS - - - - - - - - - - - - -
 
-def getWeekday(timestamp, offset):
+def get_weekday(timestamp, offset):
     weekdays = ['Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday']
     # Assuming offset is seconds, not minutes
     weekday = ((timestamp + offset) / 86400) % 7
     return weekdays[weekday]
 
 
-def getTweetPolarity(tweet, negativeWords, positiveWords):
-    wordList = tweet.translate(None, ",!.;#?:@-").lower().split(' ')
-    numPositive = len([i for i in wordList if i in positiveWords])
-    numNegative = len([i for i in wordList if i in negativeWords])
+def get_tweet_polarity(tweet, negative_words, positive_words):
+    word_list = tweet.translate(None, ",!.;#?:@-").lower().split(' ')
+    num_positive = len([i for i in word_list if i in positive_words])
+    num_negative = len([i for i in word_list if i in negative_words])
 
-    return 1 if numPositive - numNegative > 0 else -1 if numPositive - numNegative < 0 else 0
+    return 1 if num_positive - num_negative > 0 else -1 if num_positive - num_negative < 0 else 0
+
 
 # - - - - - - - - - - - - - MAIN FUNCTION - - - - - - - - - - - - -
 
@@ -28,43 +30,37 @@ if __name__ == "__main__":
 
     print('Task 2.1 - Load the twitter dataset')
 
+    # input_path = sys.argv[1]
+    # output_ath = sys.argv[2]
+    # pos_path = sys.argv[3]
+    # neg_path = sys.argv[4]
+
     # Loading files
 
+    # twitter_data = sc.textFile(input_path).map(lambda x: tuple(x.split('\t')))
     twitter_data = sc.textFile('twitter_tweets_example.tsv').map(lambda x: tuple(x.split('\t')))
     print('Tweets loaded')
 
-    '''
-    negative_words_data = sc.textFile('Twitter_data/negative-words.txt').flatMap(lambda x: x.split('\n'))
-    print('Negative words loaded')
-
-    positive_words_data = sc.textFile('Twitter_data/positive-words.txt').flatMap(lambda x: x.split('\n'))
+    # positive_words_data = sc.textFile(pos_path).collect()
+    positive_words_data = sc.textFile("hdfs://dascosa09.idi.ntnu.no:8020/user/janryb/positive-words.txt").collect()
+    positive_words_data = set(positive_words_data)
     print('Positive words loaded')
-    '''
 
-    negative_words_data = []
-    positive_words_data = []
-    with open('Twitter_data/positive-words.txt', 'r') as text_file:
-        positive_words_data = text_file.read().split('\n')
-        positive_words_data = set(positive_words_data)
-
-    with open('Twitter_data/negative-words.txt', 'r') as text_file:
-        negative_words_data = text_file.read().split('\n')
-        negative_words_data = set(negative_words_data)
-
-    print('Positive and negative words loaded')
+    # negative_words_data = sc.textFile(neg_path).collect()
+    negative_words_data = sc.textFile("hdfs://dascosa09.idi.ntnu.no:8020/user/janryb/negative-words.txt").collect()
+    negative_words_data = set(negative_words_data)
+    print('Negative words loaded')
 
     print('Task 2.2 - Find aggregated polarity of all english tweets for each city in the US for each day of week')
 
     # timestamp[0], time_offset[1], country[2], place_type[3], city[4], lang[5], tweet[6]
-    tweets = twitter_data.map(lambda t: (int(t[0]), int(t[8]), str(t[1]), str(t[3]), str(t[4]), str(t[5]), str(t[10]))) \
-        .filter(lambda row: row[2] == 'United States'
-                            and row[3] == 'city'
-                            and row[5] == 'en')
+    tweets = twitter_data.map(
+            lambda t: (int(t[0]), int(t[8]), str(t[1]), str(t[3]), str(t[4]), str(t[5]), str(t[10]))).filter(
+            lambda row: row[2] == 'United States' and row[3] == 'city' and row[5] == 'en')
 
     # city[0], dayOfWeek[1], polarityOfTweet[2]
-    tweets = tweets.map(lambda t: (str(t[4]),
-                                   str(getWeekday(t[0], t[1])),
-                                   getTweetPolarity(str(t[6]), negative_words_data, positive_words_data)))
+    tweets = tweets.map(lambda t: (
+        str(t[4]), str(get_weekday(t[0], t[1])), get_tweet_polarity(str(t[6]), negative_words_data, positive_words_data)))
 
     # TODO : make key: combine city, weekday
 
@@ -74,6 +70,6 @@ if __name__ == "__main__":
 
     print('Task 2.3 - Output result into file')
     # 'city<tab>day_of_week<tab>overall_polarity'
-    # tweets.saveAsTextFile("task2.tsv")
+    # tweets.saveAsTextFile('output_path')
 
     sc.stop()
