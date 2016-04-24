c182bdfc9633c21be4c469a75deab0e308ee611c
moynes <martheoy@stud.ntnu.no>
2016-04-24 18:43:36 +0200

    Most of the shit is kinda working now

--- twitter.py
+++ twitter.py
@@ -17,7 +17,7 @@ def tweet_char_filter(tweet):
     tweet_part = ''
     for char in tweet.lower():
         order = ord(char)
-        if 64 < order < 91 or 96 < order < 123 or c == 32 or c == 45:
+        if 64 < order < 91 or 96 < order < 123 or char == 32 or char == 45:
             tweet_part += char
         else:
             tweet_list.append(tweet_part)
@@ -31,6 +31,9 @@ def get_tweet_polarity(tweet, negative_words, positive_words):
 
     return 1 if num_positive - num_negative > 0 else -1 if num_positive - num_negative < 0 else 0
 
+
+def createCombinedKey(weekday, city):
+    return ''.join([weekday," ",city])
 
 # - - - - - - - - - - - - - MAIN FUNCTION - - - - - - - - - - - - -
 
@@ -51,6 +54,7 @@ if __name__ == "__main__":
 
     # twitter_data = sc.textFile(input_path).map(lambda x: tuple(x.split('\t')))
     twitter_data = sc.textFile('tweets_excerpt.tsv').map(lambda x: tuple(x.split('\t')))
+    # twitter_data = sc.textFile('twitter_data/geotweets.tsv').map(lambda x: tuple(x.split('\t')))
     print('Tweets loaded')
 
     # positive_words_data = sc.textFile(pos_path).collect()
@@ -66,18 +70,25 @@ if __name__ == "__main__":
     # timestamp[0], time_offset[1], country[2], place_type[3], city[4], lang[5], tweet[6]
     tweets = twitter_data.map(
             lambda t: (int(t[0]), int(t[8]), str(t[1]), str(t[3]), str(t[4]), str(t[5]), str(t[10]))).filter(
-            lambda row: row[2] == 'United States' and row[3] == 'city' and row[5] == 'en')
+            lambda t: t[2] == 'United States' and t[3] == 'city' and t[5] == 'en')
 
     # city[0], dayOfWeek[1], polarityOfTweet[2]
     tweets = tweets.map(lambda t: (
         str(t[4]), str(get_weekday(t[0], t[1])), get_tweet_polarity(str(t[6]), negative_words_data, positive_words_data)))
 
-    # TODO : make key: combine city, weekday
+    # Combine city and weekday
+    tweets = tweets.map(lambda t: (createCombinedKey(t[0], t[1]), t[2]))
 
-    # TODO : combine by key and sum polarity
+    # Sum polarity on (city, weekday)
+    tweets = tweets.combineByKey(lambda value: (value, 1),
+                             lambda x, value: (x[0] + value, x[1] + 1),
+                             lambda x, y: (x[0] + y[0], x[1] + y[1])) \
+        .map(lambda (label, (value_sum, count)): (label, value_sum))
+
+    # TODO : sort by city, weekdays
 
     for session_map in tweets.collect():
-        print("%s\t%s\t%s\n" % (session_map[0], session_map[1], session_map[2]))
+        print("%s\t%s\n" % (session_map[0], session_map[1]))
 
     print('Task 2.3 - Output result into file')
     # 'city<tab>day_of_week<tab>overall_polarity'
